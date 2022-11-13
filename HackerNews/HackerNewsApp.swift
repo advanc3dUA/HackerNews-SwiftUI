e7d20c047016458cbdaa74ec4e285c29285d2d36
advanc3dUA <fash1k@gmail.com>
2022-11-13 10:40:04 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Yuriy Gudimov on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
